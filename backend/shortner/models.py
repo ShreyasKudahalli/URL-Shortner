53a5dceee1d82d2209cb35be069bfab3c9ae40af
ShreyasKudahalli <shreyaskudahalli1857@gmail.com>
2026-07-17 22:22:08 +0530

    Create ShortURL model

--- backend/shortner/models.py
+++ backend/shortner/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class ShortURL(models.Model):
+
+    original_url = models.URLField()
+    short_code = models.CharField(max_length=100,unique=True)
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.short_code
